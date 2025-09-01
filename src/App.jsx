ae2d95bfbdd20be0b5239855af26b2e4f0d00dca
Petra Söderström <101672601+Petrasoderstrom1612@users.noreply.github.com>
2025-09-01 10:36:35 +0200

    core code

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,19 @@
-import { useState } from 'react'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import Toggle from "./components/Toggle/index"
 
 function App() {
-
   return (
     <>
-      <p>
-        Click on the Vite and React logos to learn more
-      </p>
+      <Toggle onToggle={() => {console.log("Toggled")}}>
+        <Toggle.Button>
+          <Toggle.Display>
+            {(on) => {
+              return <div className={`box ${on ? "filled" : ""}`}></div>
+            }}
+          </Toggle.Display>
+        </Toggle.Button>
+      </Toggle>
     </>
   )
 }
